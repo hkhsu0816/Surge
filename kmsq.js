1e7e591138ae32f166697600a735382dbc56e9bc
hkhsu0816 <122949127+hkhsu0816@users.noreply.github.com>
2023-09-22 14:25:19 +0800

    Update kmsq.js

--- kmsq.js
+++ kmsq.js
@@ -7,9 +7,9 @@
 [mitm]
 hostname = pay.pysmei.com
 *******************************/
-var body=$response.body;
-body = body.replace(/"viptime\":".*?"/g,'"viptime": "2099-12-31T00:00:00"');
-body = body.replace(/"isvip\":\w+/g,'"isvip":true');
-body = body.replace(/"nviptime\":".*?"/g,'"nviptime":"2099-12-31T00:00:00"');
-body = body.replace(/"isnvip\":\w+/g,'"isnvip":true');
+var body=$response.body
+.replace(/"viptime\":".*?"/g,'"viptime": "2099-12-31T00:00:00"')
+.replace(/"isvip\":\w+/g,'"isvip":true')
+.replace(/"nviptime\":".*?"/g,'"nviptime":"2099-12-31T00:00:00"')
+.replace(/"isnvip\":\w+/g,'"isnvip":true');
 $done(body);
